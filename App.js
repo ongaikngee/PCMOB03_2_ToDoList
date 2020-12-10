38ae9001da6f82fb6c33f945e86a6a9f1f729146
Glippy <jonathanong@iMac.local>
2020-12-10 12:46:27 +0800

    Created header

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import * as SQLite from 'expo-sqlite';
+import { Ionicons } from "@expo/vector-icons";
+
+const db = SQLite.openDatabase("notes.db")
+const Stack = createStackNavigator();
+
+function HomeScreen({navigation}){
+
+  useEffect(() => {
+		navigation.setOptions({
+			headerRight: () => <Button onPress={addNotes} title="Add" />,
+		});
+  });
+  
+  function addNotes(){
+    console.log("hell0");
+  }
+
+  return(
+    <View style={styles.container}>
+      <Text>Welcome</Text>
+    </View>
+  );
+
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} 
+    options={{
+      headerTitle: "Notes App",
+      headerTitleStyle: {
+        fontWeight: "bold",
+        fontSize: 30,
+      },
+      headerTintColor:"brown",
+      headerStyle: {
+        height: 120,
+        backgroundColor: "orange",
+        borderBottomColor: "#ccc",
+        borderBottomWidth: 1,
+      },
+    }}
+/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'papayawhip',
     alignItems: 'center',
     justifyContent: 'center',
   },
