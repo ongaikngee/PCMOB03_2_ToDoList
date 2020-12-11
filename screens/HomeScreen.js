c2b5acbca4182d41d1015986ef434d469a81ace3
Glippy <jonathanong@iMac.local>
2020-12-11 23:01:40 +0800

    Added ability to complate task

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,68 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, FlatList} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 import { Ionicons, AntDesign, FontAwesome5, Foundation } from '@expo/vector-icons';
 import * as SQLite from 'expo-sqlite';
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from 'expo-file-system';
 
 const db = SQLite.openDatabase('notes.db');
 
 export default function HomeScreen({ navigation, route }) {
-
 	const [ notes, setNotes ] = useState([]);
-    console.log(FileSystem.documentDirectory);
+	console.log(FileSystem.documentDirectory);
 
 	function renderItem({ item }) {
+		let BGCOLOR = '';
+		BGCOLOR = item.done ? 'gray' : 'papayawhip';
 		return (
-			<View style={styles.renderFlatlist}>
-				<Text style={{ textAlign: 'left', fontSize: 16 }}>{item.title}</Text>
-                <TouchableOpacity onPress={()=>deleteNotes(item.id)}>
-                    <AntDesign name="delete" size={24} color="black" />
-                </TouchableOpacity>
+			<View style={[ styles.renderFlatlist, { backgroundColor: BGCOLOR } ]}>
+				<TouchableOpacity onPress={() => CompleteNotes(item.id)}>
+					<Text style={{ textAlign: 'left', fontSize: 16, backgroundColor: BGCOLOR }}>{item.title}</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => deleteNotes(item.id)}>
+					<AntDesign name="delete" size={24} color="black" />
+				</TouchableOpacity>
 			</View>
 		);
-    }
-    
-    function refreshNotes() {
-        db.transaction((tx) => {
-          tx.executeSql(
-            "SELECT * FROM notes",
-            null,
-            (txObj, { rows: { _array } }) => setNotes(_array),
-            (txObj, error) => console.log(`Error: ${error}`)
-          );
-        });
-      }
+	}
 
-      function deleteNotes(id){
-        db.transaction((tx) => {
-            tx.executeSql(
-              "DELETE FROM notes WHERE id = ?",
-            [id],
-              (txObj, { rows: { _array } }) => setNotes(_array),
-              (txObj, error) => console.log(`Error: ${error}`)
-            );
-          },
-          null,
-          refreshNotes);
+	function refreshNotes() {
+		db.transaction((tx) => {
+			tx.executeSql(
+				'SELECT * FROM notes ORDER BY done',
+				null,
+				(txObj, { rows: { _array } }) => setNotes(_array),
+				(txObj, error) => console.log(`Error: ${error}`)
+			);
+		});
+	}
 
-      }
+	function deleteNotes(id) {
+		db.transaction(
+			(tx) => {
+				tx.executeSql(
+					'DELETE FROM notes WHERE id = ?',
+					[ id ],
+					(txObj, { rows: { _array } }) => setNotes(_array),
+					(txObj, error) => console.log(`Error: ${error}`)
+				);
+			},
+			null,
+			refreshNotes
+		);
+	}
 
-        // This is to set up the database on first run
-  useEffect(() => {
-    db.transaction(
-      (tx) => {
-        tx.executeSql(
-          `CREATE TABLE IF NOT EXISTS notes
+	function CompleteNotes(id) {
+		db.transaction(
+			(tx) => {
+				tx.executeSql(
+					'UPDATE notes SET done = 1 WHERE id = ?',
+					[ id ],
+					(txObj, { rows: { _array } }) => setNotes(_array),
+					(txObj, error) => console.log(`Error: ${error}`)
+				);
+			},
+			null,
+			refreshNotes
+		);
+	}
+
+	// This is to set up the database on first run
+	useEffect(() => {
+		db.transaction(
+			(tx) => {
+				tx.executeSql(
+					`CREATE TABLE IF NOT EXISTS notes
         (id INTEGER PRIMARY KEY AUTOINCREMENT,
           title TEXT,
           done INT)
         `
-        );
-      },
-      null,
-      refreshNotes
-    );
-  }, []);
+				);
+			},
+			null,
+			refreshNotes
+		);
+	}, []);
 
 	useEffect(() => {
 		navigation.setOptions({
@@ -72,37 +91,38 @@ export default function HomeScreen({ navigation, route }) {
 				</TouchableOpacity>
 			)
 		});
-    });
+	});
 
-    // Monitor route.params for changes and add items to the database
-    useEffect(() => {
-        if (route.params?.text) {
-          db.transaction(
-            (tx) => {
-              tx.executeSql("INSERT INTO notes (done, title) VALUES (0, ?)", [
-                route.params.text,
-              ]);
-            },
-            null,
-            refreshNotes
-          );
-        }
-      }, [route.params?.text]);
-
+	// Monitor route.params for changes and add items to the database
+	useEffect(() => {
+	    if (route.params?.text) {
+	      db.transaction(
+	        (tx) => {
+	          tx.executeSql("INSERT INTO notes (done, title) VALUES (0, ?)", [
+	            route.params.text,
+	          ]);
+	        },
+	        null,
+	        refreshNotes
+	      );
+	    }
+	  }, [route.params?.text]);
 
 	function addNote() {
-		navigation.navigate("Add Note");
+		navigation.navigate('Add Note');
 	}
 
 	return (
 		<View style={styles.container}>
-			<FlatList style={styles.flatlist} data={notes} renderItem={renderItem} 
-        keyExtractor={(item) => item.id.toString()} />
+			<FlatList
+				style={styles.flatlist}
+				data={notes}
+				renderItem={renderItem}
+				keyExtractor={(item) => item.id.toString()}
+			/>
 		</View>
 	);
 }
-
-
 
 const styles = StyleSheet.create({
 	container: {
@@ -117,13 +137,13 @@ const styles = StyleSheet.create({
 	},
 	renderFlatlist: {
 		padding: 10,
-        paddingTop: 20,
-        paddingHorizontal:40,
+		paddingTop: 20,
+		paddingHorizontal: 40,
 		paddingBottom: 20,
 		borderBottomColor: '#ccc',
-        borderBottomWidth: 1,
-        flexDirection:"row",
-        justifyContent:"space-between",
+		borderBottomWidth: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
 	},
 	flatlist: {
 		width: '100%'
